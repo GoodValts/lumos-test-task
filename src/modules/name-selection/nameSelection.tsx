ea7f8df200ed830c56424105a8bfbf149df5d90f
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-09-18 19:48:10 +0300

    feat: add names list from api Wed, Sep 18, 2024  7:48:10 PM

--- src/modules/name-selection/nameSelection.tsx
+++ src/modules/name-selection/nameSelection.tsx
@@ -1,31 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import radiobuttonIcon from "../../assets/icons/radiobutton-icon.png";
 
-const names = ["Name1 Surname1", "Name2 Surname2", "Name3 Surname3"];
-
 export default function NameSelection() {
-  const [selected, setSelected] = useState("Name1 Surname1");
+  const [selected, setSelected] = useState("");
+  const [names, setNames] = useState<string[]>();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        await fetch("https://randomuser.me/api/?results=10")
+          .then((res) => res.json())
+          .then((res) => res.results)
+          .then((res) => {
+            const arr = res.map(
+              (el: { name: { first: string; last: string } }) =>
+                el.name.first.concat(` ${el.name.last}`),
+            );
+            setNames(arr);
+          });
+      } catch {
+        console.error("fetch err");
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <section className={styles.section} id="vpn">
-      <p className={styles.title}>
-        Choose <span className={styles.title_blue}>your name</span>
-      </p>
-      {names.map((el) => (
-        <div className={styles.nameBlock} key={el}>
+      {names && (
+        <p className={styles.title}>
+          Choose <span className={styles.title_blue}>your name</span>
+        </p>
+      )}
+      {names &&
+        names.map((el) => (
           <div
-            className={
-              el === selected
-                ? `${styles.radioButton} ${styles.radioButton_selected}`
-                : styles.radioButton
-            }
+            className={styles.nameBlock}
+            key={el}
+            onClick={() => {
+              setSelected(el);
+            }}
           >
-            {el === selected && <img src={radiobuttonIcon.src} alt="" />}
+            <div
+              className={
+                el === selected
+                  ? `${styles.radioButton} ${styles.radioButton_selected}`
+                  : styles.radioButton
+              }
+            >
+              {el === selected && <img src={radiobuttonIcon.src} alt="" />}
+            </div>
+            <div className={styles.name}>{el}</div>
           </div>
-          <div className={styles.name}>{el}</div>
-        </div>
-      ))}
+        ))}
       <button className={styles.button}>Get VPN</button>
     </section>
   );
